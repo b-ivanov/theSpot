e925ddbd6ea12d204c26c4102b3d5349fdeeda13
Bozhidar Ivanov <bojidar.g.i@gmail.com>
2018-01-14 18:01:16 +0200

    Add files via upload

---
+++ userDocumentJSON.js
@@ -0,0 +1,124 @@
+{
+  "_id": "af359ec1e3ab74f59f26a47ef600d56d",
+  "_rev": "12-d86433c9d6dc49d436ec24b4916d814e",
+  "firstName": "Bozhidar",
+  "lastName": "Ivanov",
+  "mail": "bojo@abv.bg",
+  "birthdate": {
+    "year": 1992,
+    "month": 9,
+    "day": 23
+  },
+  "password": "asd123",
+  "places": {
+    "97310d4c-4bd5-460b-a226-5503899e8703": {
+      "name": "Public - The place to be",
+      "latitude": 48.193607,
+      "longitude": 16.352019,
+      "address": {
+        "street": "Hofmühlgasse",
+        "number": "11",
+        "city": "Vienna",
+        "postalCode": "1060"
+      },
+      "description": "Fun place to drink and meet friends!",
+      "category": [
+        "bar"
+      ],
+      "phone": "0650 5202416"
+    },
+    "ea054045-ab6b-42df-ae3e-d9eafe369e49": {
+      "name": "The Golden Harp - Irish Pub",
+      "image": "url('images/temps/image01.png')",
+      "latitude": 48.194691,
+      "longitude": 16.360925,
+      "address": {
+        "street": "Kettenbrückengasse",
+        "number": "7",
+        "city": "Vienna",
+        "postalCode": "1050"
+      },
+      "description": "Good Irish pub with darts, billiard and cards.",
+      "category": [
+        "bar"
+      ],
+      "website": "www.irishpubwien.at",
+      "phone": "0699 15871251"
+    },
+    "ea5549d2-51ba-46be-abbc-22ea0565cbd3": {
+      "name": "Prater",
+      "image": "url('images/temps/image02.jpg')",
+      "latitude": 48.216719,
+      "longitude": 16.39804,
+      "address": {
+        "city": "Vienna",
+        "postalCode": "1020"
+      },
+      "description": "Large park open 24hrs with old-time amusements including a landmark giant wheel dating from 1897.",
+      "category": [
+        "park",
+        "amusement park",
+        "entertainment"
+      ],
+      "website": "www.prater.at"
+    },
+    "ca3904de-35cb-429a-b951-aa0fafda352e": {
+      "name": "Café Central",
+      "image": "url('images/temps/image03.png')",
+      "latitude": 48.210405,
+      "longitude": 16.365425,
+      "address": {
+        "street": "Herrengasse",
+        "number": "14",
+        "city": "Vienna",
+        "postalCode": "1010"
+      },
+      "description": "Long-standing venue for Viennese cuisine, with famous historic links including to Freud & Trotsky.",
+      "category": [
+        "cafe",
+        "bar",
+        "entertainment"
+      ],
+      "website": "www.cafecentral.wien",
+      "phone": "01 5333763"
+    },
+    "36ea547b-b473-46cb-9606-e6b8563960f6": {
+      "name": "Rathaus",
+      "latitude": 48.21081,
+      "longitude": 16.357247,
+      "address": {
+        "street": "Friedrich-Schmidt-Platz",
+        "number": "1",
+        "city": "Vienna",
+        "postalCode": "1010"
+      },
+      "description": "Massive municipal building built using some 30 million bricks & now hosting balls, markets & events.",
+      "category": [
+        "entertainment"
+      ],
+      "website": "www.wien.gv.at",
+      "phone": "01 52550"
+    },
+    "40032187-ccd0-46ed-b271-43d69c9b6008": {
+      "name": "Albert Schweitzer Haus",
+      "latitude": 48.216816,
+      "longitude": 16.35669,
+      "address": {
+        "street": "Garnisongasse",
+        "number": "14-16",
+        "city": "Vienna",
+        "postalCode": "1090"
+      },
+      "description": "I live here :)",
+      "category": [
+        "home",
+        "building"
+      ]
+    },
+    "baa09b6a-5eda-49bc-af48-8eb155b41360": {
+      "name": "The Unknown",
+      "latitude": 48.207048,
+      "longitude": 16.35968
+    }
+  }
+}
